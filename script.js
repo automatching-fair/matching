1099d29a1d7eae91f0e649def0387ecd4cf7c99c
automatching-fair <154502161+automatching-fair@users.noreply.github.com>
2023-12-28 12:49:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,28 @@ function addColumn() {
   var table = document.getElementById("myTable");
   var rowCount = table.rows.length;
 
-  for (var i = 0; i < rowCount; i++) {
-    var cell = table.rows[i].insertCell(-1);
-    cell.innerHTML = '<input type="number">';
-  }
-
   var headerRow = table.rows[0];
   var newHeaderCell = document.createElement("td");
   var newHeaderNumber = document.createTextNode(columnCounter);
   newHeaderCell.appendChild(newHeaderNumber);
   headerRow.appendChild(newHeaderCell);
   columnCounter++;
+
+  for (var i = 0; i < rowCount; i++) {
+    var cell = table.rows[i].insertCell(-1);
+    if (i === 1) {
+      cell.innerHTML = '<input type="number">';
+    } else {
+      cell.innerHTML = '';
+    }
+  }
 }
 
 function addRow() {
   var table = document.getElementById("myTable");
   var rowCount = table.rows.length;
-  var row = table.insertRow(rowCount);
 
+  var row = table.insertRow(rowCount);
   for (var i = 0; i < columnCounter; i++) {
     var cell = row.insertCell(i);
     if (i === 0) {
