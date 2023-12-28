910a58b4cf4b6b7aeaa67485058540b8d8e12e7b
automatching-fair <154502161+automatching-fair@users.noreply.github.com>
2023-12-28 12:11:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function addRow() {
+function addColumn() {
   var table = document.getElementById("myTable");
-  var row = table.insertRow(-1);
-  for (var i = 0; i < table.rows[0].cells.length; i++) {
-    var cell = row.insertCell(i);
+  var rowCount = table.rows.length;
+
+  for (var i = 0; i < rowCount; i++) {
+    var cell = table.rows[i].insertCell(-1);
     cell.innerHTML = '<input type="number">';
   }
 }
@@ -10,14 +11,14 @@ function addRow() {
 function expandTable() {
   var sessionInput = document.getElementById("sessionInput").value;
   var table = document.getElementById("myTable");
-  var currentRowCount = table.rows.length;
+  var currentColumnCount = table.rows[0].cells.length - 3; // 첫번째 줄 숫자 칸 제외
 
-  if (sessionInput > 0) {
-    var expandBy = sessionInput - currentRowCount + 1;
+  if (sessionInput > currentColumnCount) {
+    var expandBy = sessionInput - currentColumnCount;
     for (var i = 0; i < expandBy; i++) {
-      addRow();
+      addColumn();
     }
   } else {
-    alert("올바른 숫자를 입력하세요.");
+    alert("현재보다 작거나 같은 숫자를 입력하세요.");
   }
 }
