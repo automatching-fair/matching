edee245db19a49415e894b881c96c9674c68b4f3
automatching-fair <154502161+automatching-fair@users.noreply.github.com>
2023-12-28 12:31:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+let rowCounter = 2;
+let columnCounter = 4;
+
 function addColumn() {
   var table = document.getElementById("myTable");
-  var headerRow = table.rows[0];
   var rowCount = table.rows.length;
 
-  var newHeaderCell = document.createElement("td");
-  var newHeaderNumber = document.createTextNode(rowCount);
-  newHeaderCell.appendChild(newHeaderNumber);
-  headerRow.appendChild(newHeaderCell);
-
-  for (var i = 1; i < rowCount; i++) {
+  for (var i = 0; i < rowCount; i++) {
     var cell = table.rows[i].insertCell(-1);
     cell.innerHTML = '<input type="number">';
   }
+
+  var headerRow = table.rows[0];
+  var newHeaderCell = document.createElement("td");
+  var newHeaderNumber = document.createTextNode(columnCounter);
+  newHeaderCell.appendChild(newHeaderNumber);
+  headerRow.appendChild(newHeaderCell);
+  columnCounter++;
 }
 
 function addRow() {
   var table = document.getElementById("myTable");
   var rowCount = table.rows.length;
-
   var row = table.insertRow(rowCount);
-  for (var i = 0; i < table.rows[0].cells.length; i++) {
+
+  for (var i = 0; i < columnCounter; i++) {
     var cell = row.insertCell(i);
-    if (rowCount === 1) {
-      cell.innerHTML = '<input type="number">';
+    if (i === 0) {
+      cell.innerHTML = rowCounter;
+      rowCounter++;
     } else {
-      cell.innerHTML = '';
+      cell.innerHTML = '<input type="number">';
     }
   }
 }
