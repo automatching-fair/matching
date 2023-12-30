66d589fad70cacb2bfa6728b6ee77422033f47bb
automatching-fair <154502161+automatching-fair@users.noreply.github.com>
2023-12-31 00:00:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ function addColumn() {
 
   for (var i = 1; i < rowCount; i++) {
     var cell = table.rows[i].insertCell(-1);
-    cell.innerHTML = '<input type="text">';
-    handleInput(cell.lastChild); // Assign event handler to the newly added input field
+    var input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    cell.appendChild(input);
+    handleInput(input); // Assign event handler to the newly added input field
   }
 }
 
@@ -23,16 +25,17 @@ function addRow() {
   var table = document.getElementById("myTable");
   var rowCount = table.rows.length;
 
-  rowCounter++;
-
   var row = table.insertRow(rowCount);
   for (var i = 0; i < columnCounter; i++) {
     var cell = row.insertCell(i);
     if (i === 0) {
       cell.innerHTML = rowCounter;
+      rowCounter++;
     } else {
-      cell.innerHTML = '<input type="text">';
-      handleInput(cell.lastChild); // Assign event handler to the newly added input field
+      var input = document.createElement('input');
+      input.setAttribute('type', 'text');
+      cell.appendChild(input);
+      handleInput(input); // Assign event handler to the newly added input field
     }
   }
 }
@@ -46,10 +49,10 @@ function handleInput(input) {
 
     // Insert each number in the cell
     for (var i = 0; i < numbers.length; i++) {
-      input.value += numbers[i].trim(); // Show all numbers without leading/trailing spaces for better user experience
+      input.value += numbers[i].trim();
 
       if (i !== numbers.length - 1) {
-        input.value += ', '; // Add commas for separation, except for the last number
+        input.value += ', ';
       }
     }
   });
