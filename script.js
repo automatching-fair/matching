a4261dc6a879cc1fceaf27c9cfa3546d12aac499
automatching-fair <154502161+automatching-fair@users.noreply.github.com>
2023-12-31 00:28:58 +0900

    Update script.js
    위의 코드에서, 새로운 행을 추가할 때 첫 번째 열에 숫자를 할당하는 부분을 변경하여 새로운 행이 2부터 시작하도록 조정했습니다. 이렇게 하면 아래로 확장하기 버튼을 눌렀을 때 새로운 행이 2부터 시작될 것입니다.

--- script.js
+++ script.js
@@ -30,13 +30,16 @@ function addRow() {
   for (var i = 0; i < columnCounter; i++) {
     var cell = row.insertCell(i);
     if (i === 0) {
-      cell.innerHTML = rowCounter; // 첫 번째 셀에 rowCounter를 할당합니다.
-      rowCounter++;
+      if (rowCount === 0) {
+        cell.innerHTML = rowCounter; // 첫 번째 행을 추가할 때 2로 설정합니다.
+      } else {
+        cell.innerHTML = rowCounter + rowCount; // 이후 행을 추가할 때 rowCounter와 rowCount를 더하여 값을 할당합니다.
+      }
     } else {
       var input = document.createElement('input');
       input.setAttribute('type', 'text');
       cell.appendChild(input);
-      handleInput(input); // Assign event handler to the newly added input field
+      handleInput(input); // 새로운 입력 필드에 이벤트 핸들러를 할당합니다.
     }
   }
 }
